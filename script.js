c1f98a555337da8fce011e48796a0d4c5d1a7090
Ramanjaneyuluaduri <ramanjaneyuluaduri62@gmail.com>
2025-02-04 22:35:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const navLinks=document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton =document.getElementById("menu-open-button");
+const menuCloseButton =document.getElementById("menu-close-button");
+
+menuOpenButton.addEventListener("click",() =>{
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click",() => menuOpenButton.click
+());
+
+navLinks.forEach(link => {
+    link.addEventListener("click",() => menuOpenButton.click());
+});
+
+//Initializing swipper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    //Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerview: 1
+        },
+        768: {
+            slidesPerview: 2
+        },
+        1024: {
+            slidesPerview: 3
+        }
+    }
+  });
